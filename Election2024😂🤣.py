337f4de334aa09f3364997baca0c3394d13772c5
Coder Vansh <153796275+Madboy11@users.noreply.github.com>
2024-06-08 10:26:15 +0530

    Election2024😂🤣.py created
    modiji jeet gaye samjo next election

---
+++ Election2024😂🤣.py
@@ -0,0 +1,9 @@
+# bjp ko 400 kya 500 seat mili hoti agar mera sampark kiya hota😂🤣😅
+# just for fun don't take is seriously
+
+other = int(input("vote of other"))
+bjp = int(input("vote of bjp"))
+ghotala = other/2
+if other>10:
+    print("vote of bjp",int(bjp)+ghotala)
+    print("vote of other",int(other)-ghotala)
